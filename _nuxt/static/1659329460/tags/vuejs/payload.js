e6d7d2f7f553b48e96103d851baa2fd8b72771ff
thddh <thddh@users.noreply.github.com>
2022-08-01 04:51:04 +0000

    deploy: 0575d625184d126e090ed10d54bccfd311df6ef2

---
+++ _nuxt/static/1659329460/tags/vuejs/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/tags/vuejs", (function(a){return {data:[{tag:{slug:"vuejs",name:a,description:"Vue.js에 대한 글들 모음입니다.",img:"vuejs.png",dir:"\u002Ftags",path:"\u002Ftags\u002Fvuejs",extension:".yaml",createdAt:"2022-08-01T04:49:53.015Z",updatedAt:"2022-08-01T04:49:53.019Z"},tagName:a,proArticles:[]}],fetch:{},mutations:void 0}}("vue.js")));
